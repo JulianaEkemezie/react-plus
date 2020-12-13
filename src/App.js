5cde7bae5e147dd1a771daa4ac324075e50d2a79
juliana <63176299+juli4ict@users.noreply.github.com>
2020-12-14 00:26:03 +0800

    added gitHub url

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
 
       <footer>
 
-      
+      Coded by  <a href ="https://www.linkedin.com/feed/" target="_blank" >Juliana Ekemezie </a> <a href="https://github.com/JulianaEkemezie/react-plus"  target="_blank" >and Open sourced on GitHub</a>
       </footer>
-    </div>
+    </div> 
   );
 }
 
